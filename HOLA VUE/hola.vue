43166507496690e286803966b617f64cb81957db
Eduardo Razo <esse_rios_e@hotmail.com>
2022-09-17 23:27:58 -0500

    Vue progresivo 22/23

---
+++ HOLA VUE/hola.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis sequi at molestiae pariatur expedita debitis nesciunt facilis quas cum libero alias natus corrupti, eligendi quis autem quia ducimus eos repudiandae!</p>
+        <div>{{ text }}</div>
+    </div>
+    
+
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                text:"Hola!"
+            }
+        }
+    };
+</script>
+
+<style>
+    p {
+        color: red;
+    }
+</style>
